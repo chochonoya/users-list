a6dba3880620c2325601f97f70e564a5449bdfdf
chochonoya <adalilovna1@gmail.com>
2023-06-08 21:09:51 +0600

    finish

--- details.js
+++ details.js
@@ -29,9 +29,57 @@ function createUser() {
   div.appendChild(birthDate);
   div.appendChild(phone);
   div.appendChild(email);
+  //card2
+  const card2 = document.createElement("div");
+  card2.classList.add("card");
+
+  const zagalovok = document.createElement("h2");
+  zagalovok.innerHTML = "characteristic";
+  const height = document.createElement("span");
+  height.innerHTML = `height: ${user.height}`;
+  const weight = document.createElement("span");
+  weight.innerHTML = `weight: ${user.weight}`;
+  const eyeColor = document.createElement("span");
+  eyeColor.innerHTML = `eyeColor: ${user.eyeColor}`;
+  const hair = document.createElement("span");
+  hair.innerHTML = `hair: ${user.hair.color}`;
+  const blood = document.createElement("span");
+  blood.innerHTML = `blood Group: ${user.bloodGroup}`;
+
+  card2.appendChild(zagalovok);
+  card2.appendChild(blood);
+  card2.appendChild(height);
+  card2.appendChild(weight);
+  card2.appendChild(hair);
+  card2.appendChild(eyeColor);
+  //card3
+  const card3 = document.createElement("div");
+  card3.classList.add("card");
+
+  const zagalovok1 = document.createElement("h2");
+  zagalovok1.innerHTML = "job information";
+  const addressCompany = document.createElement("span");
+  addressCompany.innerHTML = `address work: ${user.company.address.address}`;
+  const cityCompany = document.createElement("span");
+  cityCompany.innerHTML = `city company: ${user.company.address.city}`;
+  const department = document.createElement("span");
+  department.innerHTML = `department: ${user.company.department}`;
+  const name = document.createElement("span");
+  name.innerHTML = `name: ${user.company.name}`;
+  const title = document.createElement("span");
+  title.innerHTML = `title: ${user.company.title}`;
+
+  card3.appendChild(zagalovok1);
+  card3.appendChild(addressCompany);
+  card3.appendChild(cityCompany);
+  card3.appendChild(department);
+  card3.appendChild(name);
+  card3.appendChild(title);
 
   const userContainer = document.getElementById("userContainer");
   userContainer.appendChild(div);
+  userContainer.appendChild(card2);
+  userContainer.appendChild(card3);
 }
 
 createUser();
